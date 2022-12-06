0cbc3677c06256e499378670cdbf27c0082399b3
YesseYYesseY <YesKommerYesDig@gmail.com>
2022-12-06 05:47:45 +0100

    Add project files.

---
+++ Day1/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Day1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var Input = File.ReadAllLines("./Input.txt");
+            int MaxCalories = 0;
+            int CurrentCalories = 0;
+
+            foreach (var Line in Input)
+            {
+                if(Line == string.Empty)
+                {
+                    if(CurrentCalories > MaxCalories)
+                    {
+                        MaxCalories = CurrentCalories;
+                    }
+                    CurrentCalories = 0;
+                }
+
+                if(int.TryParse(Line, out var NewCalories))
+                {
+                    CurrentCalories += NewCalories;
+                }
+            }
+            Console.WriteLine(MaxCalories);
+        }
+    }
+}
